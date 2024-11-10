8fcc07ae49437d9cea5833e67db3b8c127a769c2
GeorgYashkin <n3v3r.er@gmail.com>
2024-11-10 22:47:32 +0500

    homework4.py

--- homework4/homework.py
+++ homework4/homework.py
@@ -1,5 +1,15 @@
-print("Введите значения a,b,c для уравнения x=a-b-4c")
-a=int(input())
-b=int(input())
-c=int(input())
-print("x = ",a-b-4*c)
+def integer(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Ошибка: Пожалуйста, введите целое число.")
+
+print("Введите значения a, b, c для уравнения x = a - b - 4c:")
+a = integer("Введите a: ")
+b = integer("Введите b: ")
+c = integer("Введите c: ")
+
+result = a - b - 4 * c
+print("x =", result)
