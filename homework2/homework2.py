ea166d1fe652fd1259661f1fd3bbf8edec9c3cbe
GeorgYashkin <n3v3r.er@gmail.com>
2024-11-10 22:49:56 +0500

    homework2.py

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,4 +1,13 @@
+def integer(prompt):
+    while True:
+        try:
+            value = int(input(prompt))
+            return value
+        except ValueError:
+            print("Ошибка: Пожалуйста, введите целое число.")
+
 print("Введите десятичное число")
-a=int(input())
-print("В двоичном виде", str(bin(a)[2:]))
-print("В восьмиричном виде",str(oct(a)[2:]))
+a = integer("Введите число: ")
+
+print("В двоичном виде:", str(bin(a)[2:]))
+print("В восьмиричном виде:", str(oct(a)[2:]))
